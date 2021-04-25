6909bd93710534c17ac020e964bb949ee246a495
Bhavesh <bhaveshjat9950@gmail.com>
2021-04-25 12:41:15 +0530

    client added

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import RsMix from "./RsMix";
+const App = () => {
+	return (
+		<>
+			<div>This is App</div>
+			<RsMix />
+		</>
+	);
+};
+
+export default App;
---
+++ client/src/RsMix.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+import store from "./store";
+
+export default function RsMix() {
+	console.log(store);
+	return <div>This is RsMix</div>;
+}
